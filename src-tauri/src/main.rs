81629ba4fa2023bfe74efb66f318faaa24f81b70
Victor Ndaba <mwangindaba@gmail.com>
2022-11-26 22:39:35 +0300

    refactor: updated main.rs file

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,10 @@
     windows_subsystem = "windows"
 )]
 
+mod file_tree;
+
+use file_tree::{get_children, get_snapshot};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -11,7 +15,7 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, get_snapshot, get_children])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
